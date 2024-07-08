9331e1ca52f179605fd2272e4a27aa6313be878b
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-07-07 21:43:30 -0400

    Feat Observer Cliente

--- src/main/java/proyecto/Cliente.java
+++ src/main/java/proyecto/Cliente.java
@@ -1,31 +1,37 @@
 package proyecto;
 
 import java.util.*;
-/**
- * La clase Cliente representa a un cliente en el juego, que realiza un pedido y notifica a los observadores cuando ha terminado de comer.
- */
-public class Cliente{
+
+public class Cliente implements Observable{
     private Pedido pedido;
     private List<Observer> observers;
     private int pedidoRandom;
-    /**
-     * Constructor para crear un nuevo Cliente.
-     *
-     * @param dificultad La dificultad del juego que afecta el pedido del cliente.
-     */
+
     public Cliente(int dificultad){
         pedidoRandom = new Random().nextInt(2) + 1;
         pedido = new Pedido(pedidoRandom,dificultad);
         pedido.setCliente(this);
         observers = new ArrayList<>();
     }
-    /**
-     * Obtiene el pedido del cliente.
-     *
-     * @return El pedido del cliente.
-     */
+
     public Pedido getPedido() {
         return pedido;
     }
 
+    public void clienteComiendo(){
+        pedido = null;
+        notifyObservers();
+    }
+
+    @Override
+    public void addObserver(Observer o){
+        observers.add(o);
+    }
+
+    @Override
+    public void notifyObservers(){
+        for(Observer observer : observers){
+            observer.update();
+        }
+    }
 }
