9a914e68005ae0c4d581a808297363457b0673ce
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-07-07 21:18:35 -0400

    Documentacion Silla

--- src/main/java/proyecto/Silla.java
+++ src/main/java/proyecto/Silla.java
@@ -1,16 +1,24 @@
 package proyecto;
-
+/**
+ * La clase Silla representa una silla en una mesa del juego, que puede estar ocupada o desocupada.
+ */
 public class Silla {
     private boolean ocupada;
-
+    /**
+     * Constructor para crear una nueva silla desocupada.
+     */
     public Silla() {
         ocupada = false;
     }
-
+    /**
+     * Ocupa la silla.
+     */
     public void ocupar() {
         ocupada = true;
     }
-
+    /**
+     * Desocupa la silla.
+     */
     public void desocupar() {
         ocupada = false;
     }
