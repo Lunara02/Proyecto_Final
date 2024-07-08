dbc5c4690f94520a1df384a9968a46dfa9516ed0
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-07-07 21:46:42 -0400

    Creacion Constructor Familia

--- src/main/java/proyecto/Familia.java
+++ src/main/java/proyecto/Familia.java
@@ -8,4 +8,14 @@ public class Familia extends Observer{
     private List<Observer> observers;
     private boolean familiaComiendo = false;
     public int integrantesComiendo = 0;
+
+    public Familia(int dificultad){
+        integrantes = new ArrayList<>();
+        observers=new ArrayList<>();
+        for(int i=0; i<random.nextInt(4)+1;i++){
+            Cliente cl = new Cliente(dificultad);
+            integrantes.add(cl);
+            cl.addObserver(this);
+        }
+    }
 }
