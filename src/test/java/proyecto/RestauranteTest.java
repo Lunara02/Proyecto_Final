6a87c736b45989b717033b935862e46a566516cc
brai44 <braian.urba@gmail.com>
2024-07-08 00:28:17 -0400

    Implementacion JUnit

---
+++ src/test/java/proyecto/RestauranteTest.java
@@ -0,0 +1,82 @@
+package proyecto;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestauranteTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void nuevosClientes() {
+    }
+
+    @Test
+    void retiradaClientes() {
+    }
+
+    @Test
+    void comiendoClientes() {
+    }
+
+    @Test
+    void tomarPedido() {
+    }
+
+    @Test
+    void sentarFamilia() {
+    }
+
+    @Test
+    void getComedor() {
+    }
+
+    @Test
+    void getCocina() {
+    }
+
+    @Test
+    void setDificultad() {
+    }
+
+    @Test
+    void setCuota() {
+    }
+
+    @Test
+    void getCuota() {
+    }
+
+    @Test
+    void limpiarClientes() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void addObserver() {
+    }
+
+    @Test
+    void notifyObserversCustom() {
+    }
+
+    @Test
+    void notifyObserversCustom1() {
+    }
+}
