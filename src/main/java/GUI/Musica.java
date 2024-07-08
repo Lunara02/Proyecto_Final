60eded1f48935b5594efe1d41d91ab30184e06ca
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-07-07 22:57:39 -0400

    Creacion Musica

---
+++ src/main/java/GUI/Musica.java
@@ -0,0 +1,21 @@
+package GUI;
+
+import javax.sound.sampled.*;
+import java.io.IOException;
+
+public class Musica {
+    private Clip clip;
+    private static Musica instance;
+    public void playMusica(String s) {
+        try {
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(getClass().getClassLoader().getResourceAsStream(s + ".wav"));
+            clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
