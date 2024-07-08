6abd1eeedb186fb1e7f5022e9a3365e81f3f1535
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-07-07 21:14:41 -0400

    Feat Pedido aleatorio

--- src/main/java/proyecto/Pedido.java
+++ src/main/java/proyecto/Pedido.java
@@ -11,6 +11,25 @@ public class Pedido {
     public Pedido(int n, int dificultad) {
         random = new Random();
         receta = new Receta();
+        numIngredientes = random.nextInt(3) + dificultad;
+        if(n == 1){
+            receta.add(Ingredientes.PAN_ABAJO);
+            for(int i=0; i<numIngredientes;i++){
+                ingrediente = random.nextInt(6)+10;
+                receta.add(Ingredientes.values()[ingrediente]);
+            }
+            receta.add(Ingredientes.PAN_ARRIBA);
+        }
+        else if(n == 2){
+            receta.add(Ingredientes.MASA);
+            for(int i=0; i<numIngredientes;i++){
+                int ingrediente = random.nextInt(7)+ 1;
+                if(!receta.getIngredientes().contains(Ingredientes.values()[ingrediente])){
+                    receta.add(Ingredientes.values()[ingrediente]);
+                }
+            }
+        }
+        Tipo = n;
     }
 
 }
