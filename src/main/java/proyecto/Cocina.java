be04814695322c33233a354fd5fd9dbfd581c488
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-07-07 22:03:38 -0400

    Feat getPedidos
    Getter de pedidos

--- src/main/java/proyecto/Cocina.java
+++ src/main/java/proyecto/Cocina.java
@@ -1,5 +1,7 @@
 package proyecto;
 
+import Excepciones.nullPedidos;
+
 import java.util.*;
 
 public class Cocina {
@@ -18,5 +20,12 @@ public class Cocina {
 
     public Pedido getPedido(){
         return pedidos.getFirst();
+    }
+
+    public ArrayList<Pedido> getPedidos() throws nullPedidos {
+        if(pedidos.isEmpty()){
+            throw new nullPedidos();
+        }
+        return pedidos;
     }
 }
