952ed5f58254a63cbcc7707c3f3561856488db7c
brai44 <braian.urba@gmail.com>
2024-07-08 00:19:28 -0400

    feat: Constructor Panel Principal

--- src/main/java/GUI/PanelPrincipal.java
+++ src/main/java/GUI/PanelPrincipal.java
@@ -7,7 +7,11 @@ import java.util.Objects;
 public class PanelPrincipal extends JFrame {
     private CardLayout cardLayout;
     private JPanel cardPanel;
+    private PanelMenu PanelMenu;
+    private PanelDificultad PanelDificultad;
     private PanelJuego PanelJuego;
+    private PanelTutorial PanelTutorial;
+    private PanelFinal PanelFinal;
 
     public PanelPrincipal() {
         setTitle("Cocina Explosiva");
@@ -17,6 +21,25 @@ public class PanelPrincipal extends JFrame {
         // Inicializa el CardLayout y el panel contenedor
         cardLayout = new CardLayout();
         cardPanel = new JPanel(cardLayout);
+
+        // Inicializa los paneles individuales
+        PanelMenu = new PanelMenu(this);
+        PanelDificultad = new PanelDificultad(this);
+        PanelJuego = new PanelJuego(this);
+        PanelTutorial = new PanelTutorial(this);
+        PanelFinal = new PanelFinal(this);
+
+        // Agrega los paneles al panel contenedor con nombres específicos
+        cardPanel.add(PanelMenu, "Menu");
+        cardPanel.add(PanelDificultad, "Dificultad");
+        cardPanel.add(PanelJuego, "Juego");
+        cardPanel.add(PanelTutorial,"Tutorial");
+        cardPanel.add(PanelFinal, "Final");
+
+        // Agrega el panel contenedor al marco y muestra el panel inicial
+        add(cardPanel);
+        cardLayout.show(cardPanel, "Menu");
+        setVisible(true);
     }
 
     public void showPanel(String panelName) {
@@ -24,5 +47,10 @@ public class PanelPrincipal extends JFrame {
         if(Objects.equals(panelName, "Juego")){
             PanelJuego.initialize();
         }
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new PanelPrincipal();
+        });
     }
 }
