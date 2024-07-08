75f9cf066f802c7df72d67565324392664974576
brai44 <braian.urba@gmail.com>
2024-07-07 22:17:19 -0400

    Creacion constructor Mesa

--- src/main/java/proyecto/Mesa.java
+++ src/main/java/proyecto/Mesa.java
@@ -8,4 +8,13 @@ public class Mesa {
     private int numMesa;
     private boolean mesaDisponible = true;
     private Familia familia;
+
+    public Mesa(int numMesa) {
+        this.numMesa = numMesa;
+        int cantidadSillas = 4;
+        sillas = new ArrayList<>(cantidadSillas);
+        for (int i = 0; i < cantidadSillas; i++) {
+            sillas.add(new Silla());
+        }
+    }
 }
