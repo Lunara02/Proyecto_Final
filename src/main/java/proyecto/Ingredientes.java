ad2237a99a0b3429061d2050653d0c08137074c9
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-07-08 00:33:16 -0400

    Documentacion Ingredientes

--- src/main/java/proyecto/Ingredientes.java
+++ src/main/java/proyecto/Ingredientes.java
@@ -1,5 +1,7 @@
 package proyecto;
-
+/**
+ * La enumeración Ingredientes representa los ingredientes disponibles en el juego, con su tipo y nombre.
+ */
 public enum Ingredientes {
     MASA("pizza", "masa"),
 
@@ -22,15 +24,30 @@ public enum Ingredientes {
 
     private String tipo;
     private String nombre;
+    /**
+     * Constructor para un ingrediente específico.
+     *
+     * @param tipo   El tipo de comida al que pertenece el ingrediente (e.g., "pizza", "hamburguesa").
+     * @param nombre El nombre del ingrediente.
+     */
     Ingredientes(String tipo, String nombre) {
         this.tipo = tipo;
         this.nombre = nombre;
     }
-
+    /**
+     * Obtiene el nombre del ingrediente.
+     *
+     * @return El nombre del ingrediente.
+     */
     public String getNombre() {
         return this.nombre;
     }
 
+    /**
+     * Devuelve una representación en cadena del ingrediente, combinando su tipo y su nombre.
+     *
+     * @return Una cadena representando el tipo y nombre del ingrediente.
+     */
     public String toString() {
         return this.tipo + " " + this.nombre;
     }
