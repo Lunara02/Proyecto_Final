23a3acc637e2f19673d163558351f4082521c182
brai44 <braian.urba@gmail.com>
2024-07-07 22:16:43 -0400

    Creacion atributos Mesa

--- src/main/java/proyecto/Mesa.java
+++ src/main/java/proyecto/Mesa.java
@@ -1,4 +1,11 @@
 package proyecto;
 
+import java.util.ArrayList;
+
 public class Mesa {
+    private int x = 0, y = 0;
+    private ArrayList<Silla> sillas;
+    private int numMesa;
+    private boolean mesaDisponible = true;
+    private Familia familia;
 }
