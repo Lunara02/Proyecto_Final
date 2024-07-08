8256aaa584b798edab4b5b1ddd719e726052bcdc
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-07-07 22:12:39 -0400

    Feat Entregar
    Metodo para entregar el pedido solicitado por los clientes retorna un booleanos en caso del que pedido sea correcto

--- src/main/java/proyecto/Cocina.java
+++ src/main/java/proyecto/Cocina.java
@@ -48,4 +48,35 @@ public class Cocina {
     public void quitarIngrediente(){
         cocinado.clear();
     }
+
+    public boolean Entregar(){
+        if(!pedidos.isEmpty()){
+            if(cocinado.equals(pedidos.getFirst().getReceta().getIngredientes()) && pedidos.getFirst().getTipo() == 1){
+                cocinado.clear();
+                puntuacion += 50;
+                return true;
+            }
+            else if(pedidos.getFirst().getTipo() == 2){
+                for(int x = 0; x < cocinado.size(); x++){
+                    if(pedidos.getFirst().getReceta().getIngredientes().contains(cocinado.get(x))){
+                        completo += 1;
+                    }
+                    else{
+                        completo -= 1;
+                    }
+                }
+                if(completo == pedidos.getFirst().getReceta().getIngredientes().size()){
+                    completo = 0;
+                    cocinado.clear();
+                    puntuacion += 50;
+                    return true;
+                }
+                else{
+                    completo = 0;
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
 }
