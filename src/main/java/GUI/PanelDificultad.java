d866fb1119347d2f02201bce09abca8253300374
brai44 <braian.urba@gmail.com>
2024-07-07 23:51:25 -0400

    Creacion constructor panel dificultad

--- src/main/java/GUI/PanelDificultad.java
+++ src/main/java/GUI/PanelDificultad.java
@@ -1,4 +1,42 @@
 package GUI;
 
-public class PanelDificultad {
+import proyecto.Restaurante;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class PanelDificultad extends JPanel{
+    public PanelDificultad(PanelPrincipal mainFrame) {
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10);
+
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                //Dificultad Facil
+                if (e.getX() >= 675 && e.getX() <= 989 && e.getY() >= 281 && e.getY() <= 372) {
+                    mainFrame.showPanel("Tutorial");
+                    Musica.getInstance().playMusica("entrada");
+                }
+                //Dificultad Medio
+                else if (e.getX() >= 667 && e.getX() <= 1007 && e.getY() >= 382 && e.getY() <= 494) {
+                    mainFrame.showPanel("Tutorial");
+                    Restaurante.getInstance().setDificultad(3);
+                    Restaurante.getInstance().setCuota(1500);
+                    Musica.getInstance().playMusica("entrada");
+                }
+                //Dificultad Dificil
+                else if (e.getX() >= 658 && e.getX() <= 1044 && e.getY() >= 503 && e.getY() <= 607) {
+                    mainFrame.showPanel("Juego");
+                    Restaurante.getInstance().setDificultad(6);
+                    Restaurante.getInstance().setCuota(2000);
+                    Musica.getInstance().playMusica("entrada");
+                }
+
+            }
+        });
+    }
 }
