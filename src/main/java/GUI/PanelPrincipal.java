90afe66ec03dc5a066b739da62df42da60badb00
brai44 <braian.urba@gmail.com>
2024-07-08 00:00:22 -0400

    feat: metodo para mostrar los paneles del juego

--- src/main/java/GUI/PanelPrincipal.java
+++ src/main/java/GUI/PanelPrincipal.java
@@ -1,4 +1,19 @@
 package GUI;
 
-public class PanelPrincipal {
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
+
+public class PanelPrincipal extends JFrame {
+    private CardLayout cardLayout;
+    private JPanel cardPanel;
+    private PanelJuego PanelJuego;
+    
+
+    public void showPanel(String panelName) {
+        cardLayout.show(cardPanel, panelName);
+        if(Objects.equals(panelName, "Juego")){
+            PanelJuego.initialize();
+        }
+    }
 }
