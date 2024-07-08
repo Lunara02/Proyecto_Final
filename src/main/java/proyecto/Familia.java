8e1db5c0a5ad8b48754f06666070ae3fa0c09000
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-07-08 00:33:28 -0400

    Documentacion Familia

--- src/main/java/proyecto/Familia.java
+++ src/main/java/proyecto/Familia.java
@@ -1,14 +1,20 @@
 package proyecto;
 
 import java.util.*;
-
+/**
+ * La clase Familia representa una familia en el juego, compuesta por varios clientes. Implementa el patrón Observable y Observer.
+ */
 public class Familia extends Observer implements Observable{
     private Random random = new Random();
     private ArrayList<Cliente> integrantes;
     private List<Observer> observers;
     private boolean familiaComiendo = false;
     public int integrantesComiendo = 0;
-
+    /**
+     * Constructor para crear una nueva Familia con un número aleatorio de integrantes.
+     *
+     * @param dificultad La dificultad del juego que afecta a los pedidos de los integrantes.
+     */
     public Familia(int dificultad){
         integrantes = new ArrayList<>();
         observers=new ArrayList<>();
@@ -18,15 +24,25 @@ public class Familia extends Observer implements Observable{
             cl.addObserver(this);
         }
     }
-
+    /**
+     * Obtiene la lista de integrantes de la familia.
+     *
+     * @return La lista de integrantes de la familia.
+     */
     public ArrayList<Cliente> getIntegrantes() {
         return integrantes;
     }
-
+    /**
+     * Verifica si toda la familia ha terminado de comer.
+     *
+     * @return {@code true} si toda la familia ha terminado de comer; {@code false} en caso contrario.
+     */
     public boolean getFamiliaComiendo(){
         return familiaComiendo;
     }
-
+    /**
+     * Método llamado cuando un integrante de la familia ha terminado de comer. Si todos los integrantes han terminado de comer, se notifica a los observadores.
+     */
     @Override
     public void update() {
         integrantesComiendo++;
@@ -35,12 +51,18 @@ public class Familia extends Observer implements Observable{
             notifyObservers();
         }
     }
-
+    /**
+     * Añade un observador a la lista de observadores de la familia.
+     *
+     * @param o El observador a añadir.
+     */
     @Override
     public void addObserver(Observer o){
         observers.add(o);
     }
-
+    /**
+     * Notifica a todos los observadores que la familia ha terminado de comer.
+     */
     @Override
     public void notifyObservers(){
         for(Observer observer : observers){
