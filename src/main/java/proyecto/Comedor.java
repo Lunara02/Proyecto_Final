014c785c67efbc6dcdd992376ab060684fedd444
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-07-08 00:33:45 -0400

    Documentacion Comedor

--- src/main/java/proyecto/Comedor.java
+++ src/main/java/proyecto/Comedor.java
@@ -1,20 +1,35 @@
 package proyecto;
 
-import java.util.ArrayList;
-
+import java.util.*;
+/**
+ * La clase Comedor representa el comedor en el juego, gestionando las mesas y asignando familias a ellas.
+ */
 public class Comedor {
     private ArrayList<Mesa> mesas = new ArrayList<>();
-
-    public Comedor(int canMesas) {
-        for (int x = 0; x < canMesas; x++) {
+    /**
+     * Constructor para crear un nuevo Comedor con una cantidad específica de mesas.
+     *
+     * @param canMesas La cantidad de mesas a crear en el comedor.
+     */
+    public Comedor(int canMesas){
+        for(int x = 0; x < canMesas; x++){
             mesas.add(new Mesa(x));
         }
     }
-
+    /**
+     * Obtiene la lista de mesas en el comedor.
+     *
+     * @return La lista de mesas.
+     */
     public ArrayList<Mesa> getMesas(){
         return mesas;
     }
-
+    /**
+     * Sienta una familia en una mesa disponible en el comedor.
+     *
+     * @param familia La familia que se quiere sentar.
+     * @return {@code true} si la familia fue sentada exitosamente; {@code false} en caso contrario.
+     */
     public boolean sentarFamilia(Familia familia){
         for(int i=0; i<mesas.size(); i++){
             if(mesas.get(i).ocuparMesa(familia)){
