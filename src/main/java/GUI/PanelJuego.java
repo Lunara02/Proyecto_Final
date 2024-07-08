576007d70829a48661cec4462d6d57af3bdef1c9
brai44 <braian.urba@gmail.com>
2024-07-07 23:53:41 -0400

    Creacion panel juego

---
+++ src/main/java/GUI/PanelJuego.java
@@ -0,0 +1,11 @@
+package GUI;
+
+import proyecto.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PanelJuego extends JPanel {
+}
