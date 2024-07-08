77929dc575bfa307768ad36b9c6055c163fc0a24
brai44 <braian.urba@gmail.com>
2024-07-07 23:54:29 -0400

    Creacion atributos Panel Juego

--- src/main/java/GUI/PanelJuego.java
+++ src/main/java/GUI/PanelJuego.java
@@ -8,4 +8,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class PanelJuego extends JPanel {
+    private PanelComedor comedor;
+    private PanelCocina cocina;
+    private Timer llegada;
+    private Timer tiempoJuego;
 }
