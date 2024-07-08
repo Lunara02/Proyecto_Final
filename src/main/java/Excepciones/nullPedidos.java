9cdca3a778de7050c77d4e2772e145baa50e4dc4
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-07-08 03:43:45 -0400

    Documentacion Excepciones
    Se documento nullPedidos

--- src/main/java/Excepciones/nullPedidos.java
+++ src/main/java/Excepciones/nullPedidos.java
@@ -1,7 +1,14 @@
 package Excepciones;
 
-public class nullPedidos extends Exception{
-    public nullPedidos(){
+/**
+ * nullPedidos es una excepción que indica que no hay pedidos en la cocina.
+ */
+public class nullPedidos extends Exception {
+
+    /**
+     * Constructor de la excepción nullPedidos.
+     */
+    public nullPedidos() {
         super();
     }
 }
