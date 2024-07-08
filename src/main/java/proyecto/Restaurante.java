c489c7b90839fb04d818bc00f79d16e00caf3844
brai44 <braian.urba@gmail.com>
2024-07-07 22:30:42 -0400

    Creacion atributos Restaurante

--- src/main/java/proyecto/Restaurante.java
+++ src/main/java/proyecto/Restaurante.java
@@ -1,4 +1,19 @@
 package proyecto;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Timer;
+
 public class Restaurante {
+    private static Restaurante instance;
+    private Comedor comedor;
+    private Cocina cocina;
+    private ArrayList<Familia> clientes;
+    private Random random = new Random();
+    private Timer clients_leaves;
+    private Timer clients_finish;
+    private List<ObserverCustom> observers;
+    private int dificultad = 0;
+    private int cuota = 1000;
 }
