1922d42a6c296c25363d703ca9ae7c7d69938cb2
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-07-07 21:13:01 -0400

    Documentacion Cliente

--- src/main/java/proyecto/Cliente.java
+++ src/main/java/proyecto/Cliente.java
@@ -1,20 +1,31 @@
 package proyecto;
 
 import java.util.*;
-
+/**
+ * La clase Cliente representa a un cliente en el juego, que realiza un pedido y notifica a los observadores cuando ha terminado de comer.
+ */
 public class Cliente{
     private Pedido pedido;
+    private List<Observer> observers;
     private int pedidoRandom;
-
+    /**
+     * Constructor para crear un nuevo Cliente.
+     *
+     * @param dificultad La dificultad del juego que afecta el pedido del cliente.
+     */
     public Cliente(int dificultad){
         pedidoRandom = new Random().nextInt(2) + 1;
         pedido = new Pedido(pedidoRandom,dificultad);
         pedido.setCliente(this);
+        observers = new ArrayList<>();
     }
-
+    /**
+     * Obtiene el pedido del cliente.
+     *
+     * @return El pedido del cliente.
+     */
     public Pedido getPedido() {
         return pedido;
     }
-
 
 }
