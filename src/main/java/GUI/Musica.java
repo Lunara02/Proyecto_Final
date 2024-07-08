cb7379a200082c6c5077ddbf2551e045f90575c8
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-07-08 00:31:54 -0400

    Documentacion Musica

--- src/main/java/GUI/Musica.java
+++ src/main/java/GUI/Musica.java
@@ -3,9 +3,20 @@ package GUI;
 import javax.sound.sampled.*;
 import java.io.IOException;
 
+/**
+ * La clase Musica se encarga de reproducir y detener música de fondo en formato WAV.
+ * Utiliza el patrón Singleton para asegurarse de que solo haya una instancia de Musica en toda la aplicación.
+ *
+ * @author Sebastian Ignacio Vega Varela
+ */
 public class Musica {
     private Clip clip;
     private static Musica instance;
+
+    /**
+     * Método que reproduce un archivo de música en formato WAV.
+     * @param s El nombre del archivo de música (sin la extensión .wav).
+     */
     public void playMusica(String s) {
         try {
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(getClass().getClassLoader().getResourceAsStream(s + ".wav"));
@@ -18,12 +29,22 @@ public class Musica {
         }
     }
 
+    /**
+     * Método que detiene la reproducción de la música y cierra el clip de audio.
+     */
     public void stopMusica() {
         if (clip != null && clip.isRunning()) {
             clip.stop();
             clip.close();
         }
     }
+
+
+    /**
+     * Obtiene la única instancia de la clase Musica.
+     * Utiliza el patrón Singleton.
+     * @return La instancia única de Musica.
+     */
     public static Musica getInstance() {
         if (instance == null) {
             instance = new Musica();
