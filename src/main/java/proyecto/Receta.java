c88354d2fcb03bddb8f43d16d922fa5920f07019
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-07-08 00:32:28 -0400

    Documentacion Receta

--- src/main/java/proyecto/Receta.java
+++ src/main/java/proyecto/Receta.java
@@ -1,17 +1,29 @@
 package proyecto;
 import java.util.ArrayList;
-
+/**
+ * La clase Receta representa una receta en el juego, compuesta por una lista de ingredientes.
+ */
 public class Receta {
     public ArrayList<Ingredientes> ingredientes;
-
+    /**
+     * Constructor para crear una nueva receta inicialmente vacía.
+     */
     public Receta() {
         ingredientes = new ArrayList<>();
     }
-
+    /**
+     * Añade un ingrediente a la receta.
+     *
+     * @param i El ingrediente a añadir a la receta.
+     */
     public void add(Ingredientes i){
         ingredientes.add(i);
     }
-        
+    /**
+     * Obtiene la lista de ingredientes de la receta.
+     *
+     * @return La lista de ingredientes de la receta.
+     */
     public ArrayList<Ingredientes> getIngredientes(){
         return ingredientes;
     }
