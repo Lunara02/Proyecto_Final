83eed4cdf7fe3438e75327b0ec7dc50e8367acf7
brai44 <braian.urba@gmail.com>
2024-07-08 00:00:38 -0400

    Creacion constructor panelprincipal

--- src/main/java/GUI/PanelPrincipal.java
+++ src/main/java/GUI/PanelPrincipal.java
@@ -8,7 +8,16 @@ public class PanelPrincipal extends JFrame {
     private CardLayout cardLayout;
     private JPanel cardPanel;
     private PanelJuego PanelJuego;
-    
+
+    public PanelPrincipal() {
+        setTitle("Cocina Explosiva");
+        setSize(1295, 759);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Inicializa el CardLayout y el panel contenedor
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+    }
 
     public void showPanel(String panelName) {
         cardLayout.show(cardPanel, panelName);
