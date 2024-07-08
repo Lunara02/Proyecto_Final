ddcafe2d68f67449ec516b9505ad40bb46cd7c77
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-07-07 22:26:10 -0400

    Creacion Comedor
    Creacion clase Comedor con variables y constructor

---
+++ src/main/java/proyecto/Comedor.java
@@ -0,0 +1,13 @@
+package proyecto;
+
+import java.util.ArrayList;
+
+public class Comedor {
+    private ArrayList<Mesa> mesas = new ArrayList<>();
+
+    public Comedor(int canMesas) {
+        for (int x = 0; x < canMesas; x++) {
+            mesas.add(new Mesa(x));
+        }
+    }
+}
